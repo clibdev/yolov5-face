2571d94890a2f70d33f342b9dba41b06b20b052f
clibdev <clibdev@gmail.com>
2023-10-24 07:35:04 +0300

    tqdm description force refresh to false

--- train.py
+++ train.py
@@ -308,7 +308,7 @@ def train(hyp, opt, device, tb_writer=None, wandb=None):
                 mem = '%.3gG' % (torch.cuda.memory_reserved() / 1E9 if torch.cuda.is_available() else 0)  # (GB)
                 s = ('%10s' * 2 + '%10.4g' * 7) % (
                     '%g/%g' % (epoch, epochs - 1), mem, *mloss, targets.shape[0], imgs.shape[-1])
-                pbar.set_description(s)
+                pbar.set_description(s, refresh=False)
 
                 # Plot
                 if plots and ni < 3:
